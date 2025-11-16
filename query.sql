28a23371df38c51ea313e07731318b78d92cf0b8
Atharv <atharvyeram11@gmail.com>
2025-11-16 16:59:59 +0530

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,7 +1,1 @@
-/*
-	Select the brand, model, condition and price from cars
-		find results where the price is less than $50,000
-*/
-
-SELECT brand, model, condition, price FROM cars
-	WHERE price < 50000;
+SELECT id, brand, model FROM cars ORDER BY id;
